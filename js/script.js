886310a43cd2a91c701b1cd01c28b8bb79f797b9
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-03-22 16:34:36 +0100

    Add: Milestone 0 and Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
+
+// Milestone 0
 let teamArray = [
   {
     name: "Wayne Barnett",
@@ -31,4 +43,8 @@ let teamArray = [
   }
 ]
 
-console.log(teamArray);
+// Milestone 1
+for(let i = 0; i < teamArray.length; i++){
+  let currentMember = teamArray[i];
+  console.log(currentMember.name, currentMember.role, currentMember.image);
+}
